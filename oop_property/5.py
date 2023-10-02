59aa267585ef68c8517753d02f1386f1f257268d
wizardx <nexuslev@gmail.com>
2023-10-02 07:25:41 +0300

    Задача 5

--- oop_property/5.py
+++ oop_property/5.py
@@ -1,10 +1,10 @@
 class WindowDlg:
     
     def __init__(self, title, width, height):
-        self.__check = {width, height}
         self.title = title
         self.width = width
         self.height = height
+        self.__flag = (self.width, self.height)
     
     @property
     def title(self):
@@ -14,7 +14,7 @@ class WindowDlg:
     def title(self, title : str):
         if isinstance(title, str):
             self.__title = title
-    
+            
     @property
     def height(self):
         return self.__height
@@ -23,6 +23,7 @@ class WindowDlg:
     def height(self, height, max = (0, 10000)):
         if max[0] <= height <= max[1]:
             self.__height = height
+            self.__check_fl()
             
     @property
     def width(self):
@@ -32,16 +33,16 @@ class WindowDlg:
     def width(self, width, max = (0, 10000)):
         if max[0] <= width <= max[1]:
             self.__width = width
-    
+            self.__check_fl()
+            
     def __check_fl(self):
-        if self.width not in self.__check or self.height not in self.__check:
-            self.show()
+        if '_WindowDlg__flag' in self.__dict__:
+            if self.__flag[0] != self.width or self.__flag[1] != self.height:
+                self.show()
+                self.__flag = (self.width, self.height)
             
     def show(self):
         print(f"{self.title}: {self.width}, {self.height}")
 
 wdl = WindowDlg('Hello', 10, 11)
-# wdl.show()
-wdl.height = 123
-wdl.width = 222
 wdl.show()
