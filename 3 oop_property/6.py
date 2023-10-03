56be40b82ba51b32ffbb2eed266133c405fdb389
wizardx <nexuslev@gmail.com>
2023-10-03 12:48:52 +0300

    Почти решил...

--- 3 oop_property/6.py
+++ 3 oop_property/6.py
@@ -27,27 +27,64 @@ class Stack:
         self.top = None
         
     def pop(self):
-        tmp = self.top.next
-        while tmp is not None:
-            if tmp.next.next == None:
-                break
-            tmp = tmp.next
+        if self.top is not None:
+            tmp = self.top
+        if tmp.next is not None:
+            while tmp.next.next is not None:
+                tmp = tmp.next
         tmp.next = None
         
     def get_data(self):
-        tmp = self.top.next
-        res : list[str] = [self.top.data]
-        while tmp.next is not None:
-            res.append(tmp.data)
-            tmp = tmp.next
+        res = list()
+        if self.top is not None:
+            tmp = self.top
+            while tmp is not None:
+                res.append(tmp.data)
+                tmp = tmp.next
         return res
     
     def push(self, obj):
-        if self.top == None:
+        if self.top is None:
             self.top = obj
         else:
-            tmp = self.top.next
+            tmp = self.top
             while tmp.next is not None:
                 tmp = tmp.next
-            else:
-                tmp.next = obj
+            tmp.next = obj
+
+                
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.push(StackObj("obj_2"))
+s.push(StackObj("obj_3"))
+s.pop()
+
+res = s.get_data()
+assert res == ["obj_1", "obj_2"], f"метод get_data вернул неверные данные: {res}"
+assert s.top == top, "атрибут top объекта класса Stack содержит неверное значение"
+
+h = s.top
+while h:
+    res = h.data
+    h = h.next
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.pop()
+assert s.get_data() == [], f"метод get_data вернул неверные данные: {s.get_data()}"
+
+n = 0
+h = s.top
+while h:
+    h = h.next
+    n += 1
+    
+assert n == 0, "при удалении всех объектов, стек-подобная стурктура оказалась не пустой"
+
+s = Stack()
+top = StackObj("name_1")
+s.push(top)
+obj = s.pop()
+assert obj == top, "метод pop() должен возвращать удаляемый объект"
